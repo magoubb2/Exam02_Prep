8c07539a2195472dab98f5e9615b5d7004087a88
mabaron- <margueritebaronbeliveau@marguereslaptop.home>
2024-04-17 15:17:22 -0400

    added ft_strcmp

--- lvl02/do_op/do_op.c
+++ lvl02/do_op/do_op.c
@@ -5,6 +5,7 @@
 
 int main(int argc, char **argv)
 {
+	// Variable that will end up being the result.
 	int res = 0;
 	int n1 = 0;
 	int n3 = 0;
@@ -13,29 +14,35 @@ int main(int argc, char **argv)
 		write(1, "\n", 1);
 		return (0);
 	}
+	// Convert the argv[1] and the argv[3] into integer.
 	n1 = atoi(argv[1]);
 	n3 = atoi(argv[3]);
+	// For additions.
 	if (argv[2][0] == '+')
 	{
 		res = n1 + n3;
 		printf("%d\n" ,res);
 	}
+	// For substractions.
 	else if (argv[2][0] == '-')
 	{
 		res = n1 - n3;
 		printf("%d\n" ,res);
 	}
+	// For multiplications.
 	else if (argv[2][0] == '*')
 	{
 		res = n1 * n3;
 		printf("%d\n" ,res);
 	}
+	// For divisions.
 	else if (argv[2][0] == '/')
 	{
 		res = n1 / n3;
 		printf("%d\n" ,res);
 
 	}
+	// For modulos.
 	else if (argv[2][0] == '%')
 	{
 		res = n1 % n3;
